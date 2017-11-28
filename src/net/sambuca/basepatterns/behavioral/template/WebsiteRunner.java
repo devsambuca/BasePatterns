02a8fa5b0e3ec94e175bcf2d2923961a45025c8c
Fominykh <stoyakovi4@gmail.com>
2017-11-28 18:04:29 +0300

    Changes in the class WebsiteRunner

--- src/net/sambuca/basepatterns/behavioral/template/WebsiteRunner.java
+++ src/net/sambuca/basepatterns/behavioral/template/WebsiteRunner.java
@@ -2,8 +2,8 @@ package net.sambuca.basepatterns.behavioral.template;
 
 public class WebsiteRunner {
     public static void main(String[] args) {
-        WelcomePage welcomePage = new WelcomePage();
-        NewsPage newsPage = new NewsPage();
+        TemplatePage welcomePage = new WelcomePage();
+        TemplatePage newsPage = new NewsPage();
 
         welcomePage.showPage();
         System.out.println("\n==============================================\n");
