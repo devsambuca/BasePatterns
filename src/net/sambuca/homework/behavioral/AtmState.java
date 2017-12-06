b52183034aa1400a45d0ae98938ff2469a37cab5
Fominykh <stoyakovi4@gmail.com>
2017-12-06 21:29:09 +0300

    Added interface AtmState

---
+++ src/net/sambuca/homework/behavioral/AtmState.java
@@ -0,0 +1,10 @@
+package net.sambuca.homework.behavioral;
+
+/**
+ * Created by smb on 06.12.2017.
+ */
+public interface AtmState {
+
+    public void widthdraw(int amount);
+    public void refill(int amount);
+}
